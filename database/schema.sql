8bb3922cb879e94b268491284031983f73ae96e5
Joseph Moberly <jmobes15@gmail.com>
2021-02-06 18:08:35 -0800

    created user route that has an endpoint to get users and an endpoint to create a new user

--- database/schema.sql
+++ database/schema.sql
@@ -1,66 +1,83 @@
--- set client_min_messages to warning;
+-- Exported from QuickDBD: https://www.quickdatabasediagrams.com/
+-- Link to schema: https://app.quickdatabasediagrams.com/#/d/bDXezQ
+-- NOTE! If you have used non-SQL datatypes in your design, you will have to change these here.
 
--- -- DANGER: this is NOT how to do it in the real world.
--- -- `drop schema` INSTANTLY ERASES EVERYTHING.
--- drop schema "public" cascade;
-
--- create schema "public";
 
 CREATE TABLE "users" (
-	"user_id" serial NOT NULL,
-	"user_name" varchar(255) NOT NULL,
-	"email" varchar(255) NOT NULL UNIQUE,
-	"password" varchar(255) NOT NULL,
-	CONSTRAINT "users_pk" PRIMARY KEY ("user_id")
-) WITH (
-  OIDS=FALSE
+    "user_id" serial   NOT NULL,
+    "user_name" varchar(255)   NOT NULL,
+    "email" varchar(255)   NOT NULL,
+    "password" varchar(255)   NOT NULL,
+    CONSTRAINT "pk_users" PRIMARY KEY (
+        "user_id"
+     ),
+    CONSTRAINT "uc_users_email" UNIQUE (
+        "email"
+    )
 );
-
-
 
 CREATE TABLE "routines" (
-	"routine_id" serial NOT NULL,
-	"date_start" DATE NOT NULL,
-	"date_finished" DATE,
-	"user_id" int NOT NULL,
-	CONSTRAINT "routines_pk" PRIMARY KEY ("routine_id")
-) WITH (
-  OIDS=FALSE
+    "routine_id" serial   NOT NULL,
+    "date_start" date   NOT NULL,
+    "date_end" date   NOT NULL,
+    "user_id" int   NOT NULL,
+    CONSTRAINT "pk_routines" PRIMARY KEY (
+        "routine_id"
+     )
 );
-
-
 
 CREATE TABLE "exercises" (
-	"exercise_id" serial NOT NULL,
-	"name" varchar(255) NOT NULL UNIQUE,
-	"favorited" BOOLEAN NOT NULL DEFAULT 'false',
-	"routine_id" int NOT NULL,
-	"user_id" int NOT NULL,
-	"highest_weight" real NOT NULL DEFAULT '0',
-	CONSTRAINT "exercises_pk" PRIMARY KEY ("exercise_id")
-) WITH (
-  OIDS=FALSE
+    "exercise_id" serial   NOT NULL,
+    "name" varchar(255)   NOT NULL,
+    CONSTRAINT "pk_exercises" PRIMARY KEY (
+        "exercise_id"
+     )
 );
 
+CREATE TABLE "routine_exercises" (
+    "routine_exercise_id" serial PRIMARY KEY,
+    "routine_id" int   NOT NULL,
+    "exercise_id" int   NOT NULL
+);
 
+CREATE TABLE "favorite_exercises" (
+    "favorite_exercise_id" serial   NOT NULL,
+    "user_id" int   NOT NULL,
+    "exercise_id" int   NOT NULL,
+    CONSTRAINT "pk_favorite_exercises" PRIMARY KEY (
+        "favorite_exercise_id"
+     )
+);
 
 CREATE TABLE "stats" (
-	"stats_id" serial NOT NULL,
-	"set" int NOT NULL DEFAULT '1',
-	"reps" int NOT NULL,
-	"weight" real NOT NULL,
-	"exercise_id" int NOT NULL,
-	CONSTRAINT "stats_pk" PRIMARY KEY ("stats_id")
-) WITH (
-  OIDS=FALSE
+    "stats_id" serial   NOT NULL,
+    "set" int   NOT NULL,
+    "reps" int   NOT NULL,
+    "weight" real   NOT NULL,
+    "exercise_id" int   NOT NULL,
+    "user_id" int   NOT NULL,
+    CONSTRAINT "pk_stats" PRIMARY KEY (
+        "stats_id"
+     )
 );
 
+ALTER TABLE "routines" ADD CONSTRAINT "fk_routines_user_id" FOREIGN KEY("user_id")
+REFERENCES "users" ("user_id");
 
+ALTER TABLE "routine_exercises" ADD CONSTRAINT "fk_routine_exercises_routine_id" FOREIGN KEY("routine_id")
+REFERENCES "routines" ("routine_id");
 
+ALTER TABLE "routine_exercises" ADD CONSTRAINT "fk_routine_exercises_exercise_id" FOREIGN KEY("exercise_id")
+REFERENCES "exercises" ("exercise_id");
 
-ALTER TABLE "routines" ADD CONSTRAINT "routines_fk0" FOREIGN KEY ("user_id") REFERENCES "users"("user_id");
+ALTER TABLE "favorite_exercises" ADD CONSTRAINT "fk_favorite_exercises_user_id" FOREIGN KEY("user_id")
+REFERENCES "users" ("user_id");
 
-ALTER TABLE "exercises" ADD CONSTRAINT "exercises_fk0" FOREIGN KEY ("routine_id") REFERENCES "routines"("routine_id");
-ALTER TABLE "exercises" ADD CONSTRAINT "exercises_fk1" FOREIGN KEY ("user_id") REFERENCES "users"("user_id");
+ALTER TABLE "favorite_exercises" ADD CONSTRAINT "fk_favorite_exercises_exercise_id" FOREIGN KEY("exercise_id")
+REFERENCES "exercises" ("exercise_id");
 
-ALTER TABLE "stats" ADD CONSTRAINT "stats_fk0" FOREIGN KEY ("exercise_id") REFERENCES "exercises"("exercise_id");
+ALTER TABLE "stats" ADD CONSTRAINT "fk_stats_exercise_id" FOREIGN KEY("exercise_id")
+REFERENCES "exercises" ("exercise_id");
+
+ALTER TABLE "stats" ADD CONSTRAINT "fk_stats_user_id" FOREIGN KEY("user_id")
+REFERENCES "users" ("user_id");
--- server/routes/users.js
+++ server/routes/users.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const db = require('../db');
 const HttpError = require('../models/Http-Error');
+const validate = require('../validation/validate');
 const router = express.Router();
 
 router.get('/', async (req, res, next) => {
@@ -12,6 +13,29 @@ router.get('/', async (req, res, next) => {
   }
 
   res.status(200).send(users.rows);
+});
+
+router.post('/', async (req, res, next) => {
+  const { error } = validate(req.body);
+  if (error) {
+    return next(new HttpError(error.details[0].message, 400));
+  }
+  const { username, email, password } = req.body;
+
+  let user;
+  try {
+    user = await db.query(`INSERT INTO users (user_name, email, password)
+    VALUES($1, $2, $3) RETURNING *`, [username, email, password]);
+  } catch (ex) {
+    let message, code;
+    if (ex.code === '23505') {
+      message = 'Email already exists';
+      code = 400;
+    }
+    return next(new HttpError(message || 'Unexpected error', code || 500));
+  }
+
+  res.status(201).send(user.rows);
 });
 
 module.exports = router;
