9e875aab1ce5db1385b2555f831626cf3a72907f
Joseph Moberly <jmobes15@gmail.com>
2021-02-08 00:47:59 -0800

    Finished login page styling

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -11,6 +11,13 @@ const Login = () => {
         <span className="title__text title__text--2">T</span>
       </h1>
       <LoginForm />
+      <p className="login__guest">
+        Login as <span className="login__guest--text">GUEST</span>
+      </p>
+      <p className="link__signup">
+        Don't have an account?{" "}
+        <span className="link__signup--text">Signup here</span>
+      </p>
     </>
   );
 };
