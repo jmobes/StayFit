f3c8e419c5d5d892f509ad0ffa05ae8747cadfec
Joseph Moberly <jmobes15@gmail.com>
2021-02-09 20:29:40 -0800

    created button that will display exercises when clicked and the container where the exercise data will be shown

--- frontend/src/pages/Workout.jsx
+++ frontend/src/pages/Workout.jsx
@@ -1,10 +1,12 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Workout.css";
 
 import HeaderButton from "../components/HeaderButton";
 import FitnessCenterIcon from "@material-ui/icons/FitnessCenter";
+import AddIcon from "@material-ui/icons/Add";
 
 const Workout = () => {
+  const [displayExercises, setDisplayExercises] = useState(false);
   return (
     <div className="workout">
       <div className="workout__header">
@@ -15,6 +17,11 @@ const Workout = () => {
         <FitnessCenterIcon className="workout__icon" style={{ fontSize: 60 }} />
         <h2 className="workout__title__text">workout</h2>
       </div>
+      <div className="workout__select">
+        <p className="workout__select__title">select exercise</p>
+        <AddIcon className="workout__select__icon" style={{ fontSize: 30 }} />
+      </div>
+      <div className="workout__data">No exercise data</div>
     </div>
   );
 };
