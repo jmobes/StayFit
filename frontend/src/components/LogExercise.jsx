0cf2ee0be64f71c273c887dd9a5a7322028bc984
Joseph Moberly <jmobes15@gmail.com>
2021-02-16 20:58:05 -0800

    finished the exercise log page styling. Also added state so that the page conditionally renders rows of inputs that match the state.

--- frontend/src/components/LogExercise.jsx
+++ frontend/src/components/LogExercise.jsx
@@ -1,9 +1,12 @@
 import React, { useState, useEffect } from "react";
 import "./LogExercise.css";
 
+import Inputs from "./Inputs";
 import AddIcon from "@material-ui/icons/Add";
 
 const LogExercise = (props) => {
+  const [numSets, setNumSets] = useState([1]);
+
   return (
     <div className="log__exercise">
       <h3 className="log__exercise__title">Bench Press</h3>
@@ -14,26 +17,18 @@ const LogExercise = (props) => {
           <h4 className="log__exercise__data__column">Reps</h4>
         </div>
         <div className="log__exercise__data__rows">
-          <div className="log__exercise__data__row">
-            <input
-              placeholder="set"
-              type="text"
-              className="log__exercise__data__row__item"
-            />
-            <input
-              placeholder="weight"
-              type="text"
-              className="log__exercise__data__row__item"
-            />
-            <input
-              placeholder="reps"
-              type="text"
-              className="log__exercise__data__row__item"
-            />
-          </div>
+          {numSets.map((set) => (
+            <Inputs key={set} set={set} />
+          ))}
         </div>
       </div>
-      <AddIcon style={{ fontSize: 50 }} className="add__set__button" />
+      <AddIcon
+        onClick={() =>
+          setNumSets((prev) => [...prev, numSets[numSets.length - 1] + 1])
+        }
+        style={{ fontSize: 50 }}
+        className="add__set__button"
+      />
       <button className="log__exercise__button">Log Exercise</button>
       <button className="cancel__exercise__button">Cancel</button>
     </div>
