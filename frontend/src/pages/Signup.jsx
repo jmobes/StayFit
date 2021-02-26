7b2c5774bc95059fd3fd4cf123909e07694459e8
Joseph Moberly <jmobes15@gmail.com>
2021-02-26 15:37:17 -0800

    created input validation on signup component. Also, redirects user back to the login page when the signup button is clicked. Used native html input required attributes to ensure that a user must fill out all input fields. Implemented a check on the front end just in case the html required attributes somehow don't work.

--- frontend/src/pages/Signup.jsx
+++ frontend/src/pages/Signup.jsx
@@ -1,18 +1,42 @@
 import React, { useState } from "react";
+import { withRouter, Link } from "react-router-dom";
 
 import "./Signup.css";
 import PersonIcon from "@material-ui/icons/Person";
 import MailOutlineIcon from "@material-ui/icons/MailOutline";
 import VpnKeyIcon from "@material-ui/icons/VpnKey";
 
-const Signup = () => {
+const Signup = (props) => {
   const [username, setUsername] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [usernameBlur, setUsernameBlur] = useState(false);
+  const [emailBlur, setEmailBlur] = useState(false);
+  const [passwordBlur, setPasswordBlur] = useState(false);
+  const [error, setError] = useState("");
 
-  const handleSubmit = (e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault();
-    console.log("User has signed up");
+    if (!username || !email || !password) {
+      setError("Please fill out all fields");
+      return;
+    }
+    const options = {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ username, email, password }),
+    };
+
+    try {
+      const res = await fetch("http://localhost:5000/api/users", options);
+      const user = await res.json();
+      if (!res.ok) {
+        throw new Error(user);
+      }
+      props.history.push("/");
+    } catch (err) {
+      setError(err.message);
+    }
   };
 
   return (
@@ -22,7 +46,13 @@ const Signup = () => {
         <span className="title__text title__text--2">T</span>
       </h1>
       <form onSubmit={handleSubmit} className="form__signup">
-        <div className="form__signup__username">
+        <div
+          className={`${
+            !username && usernameBlur
+              ? "form__signup__username form__signup__error"
+              : "form__signup__username"
+          }`}
+        >
           <PersonIcon
             className="icon-person"
             style={{ fontSize: 30, color: "#fff" }}
@@ -34,9 +64,16 @@ const Signup = () => {
             onChange={(e) => setUsername(e.target.value)}
             value={username}
             required
+            onBlur={() => setUsernameBlur(true)}
           />
         </div>
-        <div className="form__signup__email">
+        <div
+          className={`${
+            !email && emailBlur
+              ? "form__signup__email form__signup__error"
+              : "form__signup__email"
+          }`}
+        >
           <MailOutlineIcon
             className="icon-email"
             style={{ fontSize: 30, color: "#fff" }}
@@ -48,9 +85,16 @@ const Signup = () => {
             onChange={(e) => setEmail(e.target.value)}
             value={email}
             required
+            onBlur={() => setEmailBlur(true)}
           />
         </div>
-        <div className="form__signup__password">
+        <div
+          className={`${
+            !password && passwordBlur
+              ? "form__signup__password form__signup__error"
+              : "form__signup__password"
+          }`}
+        >
           <VpnKeyIcon
             className="icon-password"
             style={{ fontSize: 30, color: "#fff" }}
@@ -62,12 +106,17 @@ const Signup = () => {
             onChange={(e) => setPassword(e.target.value)}
             value={password}
             required
+            onBlur={() => setPasswordBlur(true)}
           />
         </div>
+        {error && <p className="signup__error">{error}</p>}
         <input type="submit" value="Signup" className="button__signup" />
       </form>
+      <Link to="/login">
+        <p className="signup__cancel">Back to Login</p>
+      </Link>
     </>
   );
 };
 
-export default Signup;
+export default withRouter(Signup);
