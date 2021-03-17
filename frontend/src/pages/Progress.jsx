c092e20633d39fde6a6e888ddd83f261de7361db
Joseph Moberly <jmobes15@gmail.com>
2021-03-16 22:58:36 -0700

    added the home and logout buttons to the progress page. Both buttons work as expected. Also imported the page logo and centered it

--- frontend/src/pages/Progress.jsx
+++ frontend/src/pages/Progress.jsx
@@ -1,7 +1,21 @@
 import React from "react";
 
+import "./Progress.css";
+import HeaderButton from "../components/HeaderButton";
+import TimelineIcon from "@material-ui/icons/Timeline";
+
 const Progress = (props) => {
-  return <div>PROGRESS PAGE</div>;
+  return (
+    <div className="progress">
+      <div className="progress__header">
+        <HeaderButton text="home" />
+        <HeaderButton text="logout" logout={props.logout} />
+      </div>
+      <div className="progress__title">
+        <TimelineIcon style={{ fontSize: 100 }} />
+      </div>
+    </div>
+  );
 };
 
 export default Progress;
