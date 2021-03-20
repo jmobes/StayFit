08572b05ad64d613b6cf5bea42331c3252dd9199
Joseph Moberly <jmobes15@gmail.com>
2021-03-20 02:10:28 -0700

    created chart component that uses chart.js to dynamically create a line chart with data from api

--- frontend/src/components/Chart.jsx
+++ frontend/src/components/Chart.jsx
@@ -1,75 +1,68 @@
 import React, { useState, useEffect } from "react";
 import { Line } from "react-chartjs-2";
+import Spinner from "./Spinner";
 
 const Chart = (props) => {
-  const [label, setLabel] = useState([]);
-  const [points, setPoints] = useState([]);
-  const [info, setInfo] = useState();
+  const [chartData, setChartData] = useState();
   const [error, setError] = useState();
 
-  useEffect(async () => {
+  const chart = () => {
     const userId = props.exercise.user_id;
     const exerciseId = props.exercise.exercise_id;
-    const exercise = props.exercise.name;
+    const weights = [];
+    const dates = [];
 
-    try {
-      const result = await fetch(
-        `http://localhost:5000/api/progress/${userId}/${exerciseId}`
-      );
-      const res = await result.json();
-      if (res.length <= 1)
-        throw new Error("Exercise needs at least two data points");
+    setChartData(null);
+    setError(null);
 
-      const dates = [];
-      const data_points = [];
-      res.map((stats) => {
-        const date = new Date(stats.date_end);
-        const formattedDate =
-          date.getFullYear() +
-          "-" +
-          (date.getMonth() + 1) +
-          "-" +
-          date.getDate();
-        dates.push(formattedDate);
-        data_points.push(stats.max);
-      });
-      setLabel([...label, dates]);
-      setPoints([...points, data_points]);
-      const info = {
-        labels: label,
-        datasets: [
-          {
-            label: "Weight",
-            fill: false,
-            lineTension: 0.4,
-            backgroundColor: "#27e77f",
-            borderColor: "#27e77f",
-            borderWidth: 2,
-            data: points,
-          },
-        ],
-      };
-      setInfo(info);
-    } catch (ex) {
-      setError(ex.message);
-      console.error(ex.message);
-    }
-  }, []);
+    fetch(`http://localhost:5000/api/progress/${userId}/${exerciseId}`)
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        data.map((stats) => {
+          const date = new Date(stats.date_end);
+          const formattedDate =
+            date.getMonth() +
+            1 +
+            "-" +
+            date.getDate() +
+            "-" +
+            date.getFullYear();
+          dates.push(formattedDate);
+          weights.push(stats.max);
+        });
+        setChartData({
+          labels: dates,
+          datasets: [
+            {
+              label: "weight",
+              data: weights,
+              backgroundColor: "#27e77f",
+              borderColor: "#27e77f",
+              borderWidth: 2,
+              fill: false,
+            },
+          ],
+        });
+      })
+      .catch((err) => console.error(err));
+  };
+
+  useEffect(() => {
+    chart();
+    console.log("CHART DATA: ", chartData);
+  }, [props.exercise]);
 
   return (
     <div className="graph">
-      {label && (
+      {chartData && (
         <Line
-          data={info}
+          data={chartData}
           options={{
             title: {
               display: true,
-              text: "Exercise Progress",
+              text: `${props.exercise.name.toUpperCase()}`,
               fontSize: 20,
-            },
-            legend: {
-              display: true,
-              position: "right",
             },
             responsive: true,
             maintainAspectRatio: false,
