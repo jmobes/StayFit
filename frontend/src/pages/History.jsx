75782f9d10f5770fd513ae804a2558f9ad1dae07
Joseph Moberly <jmobes15@gmail.com>
2021-03-21 13:28:44 -0700

    added custom styles to the react-calendar to make the colors a better fit for the app

--- frontend/src/pages/History.jsx
+++ frontend/src/pages/History.jsx
@@ -1,8 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
+
+import Calendar from "react-calendar";
 import "./History.css";
+import HistoryIcon from "@material-ui/icons/History";
+import HeaderButton from "../components/HeaderButton";
 
 const History = (props) => {
-  return <div className="history">HISTORY PAGE</div>;
+  const [value, onChange] = useState(new Date());
+  return (
+    <div className="history">
+      <div className="history__header">
+        <HeaderButton text="home" />
+        <HeaderButton text="logout" logout={props.logout} />
+      </div>
+      <div className="history__icon">
+        <HistoryIcon style={{ fontSize: 100 }} />
+      </div>
+      <div className="history__calendar">
+        <Calendar
+          onChange={onChange}
+          value={value}
+          className="bg-gray"
+          tileClassName="bg-green"
+        />
+      </div>
+    </div>
+  );
 };
 
 export default History;
